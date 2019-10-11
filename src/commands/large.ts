544d590b7ba7540e4bc8f14aba80e2e0669effbc
Dylan Stankievech <dylan.stank@gmail.com>
2019-10-10 21:08:26 -0600

    Try using large test finder import

--- src/commands/large.ts
+++ src/commands/large.ts
@@ -1,22 +1,33 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command'
+
+import { getLargeTests } from '../lib/largeTestFinder'
 
 export default class Large extends Command {
-  static description = 'Find mocha tests that have too many lines of code'
+    static description = 'Find mocha tests that have too many lines of code'
 
-  static flags = {
-    help: flags.help({char: 'h'}),
-    dir: flags.string({char: 'd', description: 'directory containing tests to search'}),
-    lines: flags.integer({char: 'l', description: 'number of lines of test code that is too many'}),
-    top: flags.integer({char: 'd', description: 'directory containing tests to search'}),
-  }
+    static examples = [
+        `$ btf large --dir=/some/test/dir --lines=50 --top=20
+# outputs top 20 tests from the given directory that have at least 50 lines
+`,
+    ]
 
-  async run() {
-    const {flags} = this.parse(Large)
+    static flags = {
+        help: flags.help({ char: 'h' }),
+        dir: flags.string({ char: 'd', description: 'directory containing tests to search' }),
+        lines: flags.integer({ char: 'l', description: 'number of lines of test code that is too many' }),
+        top: flags.integer({ char: 'd', description: 'directory containing tests to search' }),
+    }
 
-    const lines = flags.lines || 20
-    const dir = flags.dir || '.'
-    const top = flags.top || 20
+    async run() {
+        const { flags } = this.parse(Large)
 
-    this.log(`you input --dir: ${dir} --lines: ${lines} --top: ${top}`)
-  }
+        const lines = flags.lines || 20
+        const dir = flags.dir || '.'
+        const top = flags.top || 20
+
+        this.log(`you input --dir: ${dir} --lines: ${lines} --top: ${top}`)
+
+        const tests = getLargeTests(dir, lines, top)
+        this.log(tests)
+    }
 }
