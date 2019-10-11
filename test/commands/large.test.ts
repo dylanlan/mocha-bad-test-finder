0332b6ea37444f0e90562b7fd505e9d13796a92d
Dylan Stankievech <dylan.stank@gmail.com>
2019-10-10 21:45:41 -0600

    Added a large test

--- test/commands/large.test.ts
+++ test/commands/large.test.ts
@@ -1,17 +1,43 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from '@oclif/test'
 
 describe('large', () => {
-  test
-    .stdout()
-    .command(['large'])
-    .it('runs hello', ctx => {
-      expect(ctx.stdout).to.contain('hello world')
-    })
+    test
+        .stdout()
+        .command(['large'])
+        .it('runs hello', ctx => {
+            expect(ctx.stdout).to.contain('hello world')
+        })
 
-  test
-    .stdout()
-    .command(['large', '--name', 'jeff'])
-    .it('runs hello --name jeff', ctx => {
-      expect(ctx.stdout).to.contain('hello jeff')
-    })
+    test
+        .stdout()
+        .command(['large', '--name', 'jeff'])
+        .it('runs hello --name jeff', ctx => {
+            expect(ctx.stdout).to.contain('hello jeff')
+            let test = 3
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            test++
+            expect(test).to.eq(3)
+        })
 })
