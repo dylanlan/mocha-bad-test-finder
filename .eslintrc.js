b95ff21dfd195732420869d58f1e44c112a40efa
Dylan Stankievech <dylan.stank@gmail.com>
2020-06-17 00:15:53 -0600

    Add some more recommended eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,22 @@
 module.exports = {
-  extends: ['airbnb-typescript/base'],
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:chai-friendly/recommended',
+    'plugin:promise/recommended',
+    'plugin:unicorn/recommended',
+  ],
   plugins: [
     '@typescript-eslint',
     'promise',
     'unicorn',
+    'chai-friendly',
   ],
   parserOptions: {
     project: './tsconfig.json',
   },
   rules: {
     '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-unused-expressions': 0,
+    'chai-friendly/no-unused-expressions': 2
   },
 };
